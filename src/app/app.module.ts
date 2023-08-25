ef2a21ab5e16568d0b2422f088521d343c747ebf
Muhammad-Sajjad <sajjad.rizvi@apimatic.io>
2023-08-25 23:56:56 +0500

    adds dummy task cards

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { NbThemeModule, NbLayoutModule, NbSidebarModule } from '@nebular/theme';
+import { NbThemeModule, NbLayoutModule, NbSidebarModule, NbCardModule, NbIconModule } from '@nebular/theme';
 import { NbEvaIconsModule } from '@nebular/eva-icons';
 import { TaskCardsSectionComponent } from './task-cards-section/task-cards-section.component';
 
@@ -21,6 +21,8 @@ import { TaskCardsSectionComponent } from './task-cards-section/task-cards-secti
     NbLayoutModule,
     NbEvaIconsModule,
     NbSidebarModule.forRoot(),
+    NbCardModule,
+    NbIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
